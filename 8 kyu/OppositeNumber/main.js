310fe3ea71dd363d7ade56f535386b1293a24b03
KhrystynaDenysiuk <khrystyna_denysiuk@ukr.net>
2023-10-16 19:04:44 +0300

    Completed the level 8kyu task: Opposite number

---
+++ 8 kyu/OppositeNumber/main.js
@@ -0,0 +1,7 @@
+/*
+  Very simple, given an integer or a floating-point number, find its opposite.
+*/
+
+const opposite = (number) => (number === 0 ? number : -number);
+
+console.log(opposite(-7));
