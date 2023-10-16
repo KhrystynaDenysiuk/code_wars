7c8554338d0d92b94d6840e13200468c13dd1dc2
KhrystynaDenysiuk <khrystyna_denysiuk@ukr.net>
2023-10-16 19:41:30 +0300

    Completed the level 8kyu task: Grasshopper - Summation

---
+++ 8 kyu/Summation/main.js
@@ -0,0 +1,19 @@
+/*
+  Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+*/
+
+// const summation = (num) => {
+//   let totalSum = 0;
+
+//   for (let i = 1; i <= num; i++) {
+//     totalSum += i;
+//   }
+
+//   return totalSum;
+// };
+
+// or
+
+const summation = (num) => (num * (num + 1)) / 2;
+
+console.log("fun", summation(8));
